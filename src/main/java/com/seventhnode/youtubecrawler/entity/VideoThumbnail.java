221179bb5f8f3d0b4c9c1e8609ed2d5c1e50d778
nikotina <84851354+nikotina@users.noreply.github.com>
2022-01-19 11:38:40 +0100

    Youtube-dl Classes

---
+++ src/main/java/com/seventhnode/youtubecrawler/entity/VideoThumbnail.java
@@ -0,0 +1,9 @@
+package com.seventhnode.youtubecrawler.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class VideoThumbnail {
+    public String url;
+    public String id;
+}
--- youtube-crawler/src/main/java/com/seventhnode/youtubecrawler/service/YoutubeApiService.java
+++
@@ -1,5 +0,0 @@
-package com.seventhnode.youtubecrawler.service;
-
-public interface YoutubeApiService {
-    void crawlYoutubeVideoInfo(String keyword, long pageToCrawl);
-}
