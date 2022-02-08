d1d7adce773bc87eef3222f25ef256869ab7c339
nikotina <84851354+nikotina@users.noreply.github.com>
2022-02-08 20:05:06 +0100

    cleanup

--- src/main/java/com/seventhnode/youtubecrawler/api/YoutubeController.java
+++ src/main/java/com/seventhnode/youtubecrawler/api/YoutubeController.java
@@ -3,11 +3,7 @@ package com.seventhnode.youtubecrawler.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.seventhnode.youtubecrawler.entity.CrawlingInfo;
 import com.seventhnode.youtubecrawler.entity.YouTubeVideoInfo;
@@ -44,7 +40,7 @@ public class YoutubeController {
     public void crawlVideo(@PathVariable String keyword, @PathVariable long pageToCrawl) {
          youtubeApiService.crawlYoutubeVideoInfo(keyword,pageToCrawl);
     }
-    
+    @CrossOrigin
     @GetMapping(value = "crawl")
     public List<CrawlingInfo> getAllCrawlingInfo() {
         return crawlingInfoService.getAll();
