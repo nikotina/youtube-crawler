f7ba22d3b6e51629be7cecfc71160b42e23dff6f
nikotina <84851354+nikotina@users.noreply.github.com>
2022-06-27 20:37:31 +0200

    cleanup

--- src/main/java/com/seventhnode/youtubecrawler/api/YoutubeDownloadController.java
+++ src/main/java/com/seventhnode/youtubecrawler/api/YoutubeDownloadController.java
@@ -12,10 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 /**
@@ -27,7 +26,7 @@ public class YoutubeDownloadController {
     @Autowired
     private Environment env;
 
-    public static final Map<String, SseEmitter> emitters = Collections.synchronizedMap(new HashMap<String, SseEmitter>());
+    public static final Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
 
     /**
      * Download YouTube Video
@@ -37,14 +36,17 @@ public class YoutubeDownloadController {
      */
     @CrossOrigin
     @RequestMapping(path = "youtube/youtubedownload/{videoId}", method = RequestMethod.GET)
-    public void download(@PathVariable String videoId) {
+    public void download(@PathVariable String videoId) throws IOException {
         String videoUrl = "http://www.youtube.com/watch?v=" + videoId;
 
 
         String execDirectory = env.getProperty("youtube.executable");
         String downloadDirectory = env.getProperty("youtube.downloadDirectory");
-        SseEmitter emitter = new SseEmitter();
-        emitters.put(videoId, emitter);
+        SseEmitter sseEmitter = new SseEmitter();
+        sseEmitters.put(videoId, sseEmitter);
+        sseEmitter.send(SseEmitter.event().name("VideoID").data(videoId));
+        sseEmitter.onCompletion(() -> sseEmitters.remove(videoId));
+        sseEmitter.onTimeout(() -> sseEmitters.remove(videoId));
 
         YoutubeDLRequest request = new YoutubeDLRequest(videoUrl, execDirectory);
         request.setOption("ignore-errors");        // --ignore-errors
@@ -57,7 +59,7 @@ public class YoutubeDownloadController {
                 public void onProgressUpdate(float progress, long etaInSeconds) {
                     try {
                         //Logger.log(Level.FINE, progress + "%");
-                        emitter.send(progress + "%", MediaType.APPLICATION_JSON);
+                        sseEmitter.send(progress, MediaType.APPLICATION_JSON);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -68,20 +70,20 @@ public class YoutubeDownloadController {
             System.out.println(stdOut);
         } catch (YoutubeDLException dlException) {
             System.out.println(dlException.getMessage());
-            emitter.completeWithError(dlException);
+            sseEmitter.completeWithError(dlException);
         }
-        emitter.complete();
+        sseEmitter.complete();
     }
 
     @GetMapping(value = "progress/{videoId}")
     public SseEmitter progress(PathVariable videoId) {
-        SseEmitter emitter = emitters.get(videoId);
+        SseEmitter emitter = sseEmitters.get(videoId);
         return emitter;
     }
+
     @GetMapping(value = "emitters")
-    public Set emitters(PathVariable videoId) {
-        Set<String> videoIdList =  emitters.keySet();
+    public Set emitters() {
+        Set<String> videoIdList = sseEmitters.keySet();
         return videoIdList;
     }
-
 }
